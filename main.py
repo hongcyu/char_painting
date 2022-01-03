2f82f20d4b9dc1438ccfcd223770f3e37fe0e415
rollingQP <59548978+rollingQP@users.noreply.github.com>
2022-01-03 19:32:07 +0800

    一些错误处理机制、清除临时文件功能
    1、如果没有安装opencv和Pillow，将会自动使用pip从阿里云安装。
    2、加入了删除临时文件的功能。
    3、如果没有找到默认的待处理视频（input.mp4)，将会询问视频名称。另外，运行脚本时可以把视频路径以参数形式直接传入，更加省事！
    4、对ffmpeg命令加入-y参数，强制覆盖已经出现的旧输出视频。

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import sys
-from multiprocessing import Process
-import cv2
-from PIL import Image, ImageFont, ImageDraw
 import os
-from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
 import time
+import shutil
+from multiprocessing import Process
+
+try:
+    from PIL import Image, ImageFont, ImageDraw
+except ImportError:
+    os.system('pip install Pillow -i https://mirrors.aliyun.com/pypi/simple/')
+    from PIL import Image, ImageFont, ImageDraw
+
+try:
+    import cv2
+    from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
+except ImportError:
+    os.system('pip install opencv-python-headless -i https://mirrors.aliyun.com/pypi/simple/')
+    import cv2
+    from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
+
 
 
 # =========================
 # coding:UTF-8
-# 视频转字符画含音频version-1
+# 视频转字符画含音频version-2.1
 # 参考1：https://blog.csdn.net/mp624183768/article/details/81161260
 # 参考2：https://blog.csdn.net/qq_42820064/article/details/90958577
 # 参考3：https://blog.csdn.net/zj360202/article/details/79026891
 # =========================
+
+
 def get_char(r, g, b, alpha=256):
     ascii_char = list("#RMNHQODBWGPZ*@$C&98?32I1>!:-;. ")
     # ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:oa+>!:+. ")
@@ -41,7 +56,7 @@ def video_to_pic(vp):
         number += 1
         cv2.imwrite(sys.path[0] + '/cache_pic/' + str(number) + '.jpg', frame)
         r, frame = vp.read()
-    print('\n由视频一共生成了{}张图片！'.format(number))
+    print('由视频一共生成了{}张图片！'.format(number))
     os.chdir(sys.path[0])
     # os.chdir("../../../Downloads")
     return number
@@ -142,7 +157,7 @@ def star_to_char2(number, save_pic_path, start_number, end_number):
 
 
 def star_to_char_multi_process(number, save_pic_path, process_number):
-    print("正在把图片转字符画，请稍候...")
+    print("\n正在把图片转字符画，请稍候...")
     print("启动多进程处理：")
     processes = []
     for count in range(1, process_number + 1):
@@ -170,7 +185,7 @@ def process_bar(percent, start_str='', end_str='', total_length=0):
 
 
 def jpg_to_video(char_image_path, FPS):
-    print("开始合成视频")
+    print("\n开始合成视频")
     video_fourcc = VideoWriter_fourcc(*"MP42")  # 设置视频编码器,这里使用使用MP42编码器,可以生成更小的视频文件
     char_img_path_list = [char_image_path + r'/{}.jpg'.format(i) for i in range(1, number + 1)]  # 生成目标字符图片文件的路径列表
     char_img_test = Image.open(char_img_path_list[1]).size  # 获取图片的分辨率
@@ -198,20 +213,43 @@ def write_audio(video_path):
     # 加入音频
     cmd = 'ffmpeg -i ' + sys.path[
         0] + '/video/new_char_video.avi' + ' -i ' + video_path + ' -c copy -map 0 -map 1:1 -y -shortest ' + sys.path[
-              0] + '/video/videoWithAudio.avi'
+              0] + '/video/videoWithAudio.avi' + ' -y'
     os.system(cmd)
     # 压制成H.264 mp4格式
     cmd2 = 'ffmpeg -i ' + sys.path[0] + '/video/videoWithAudio.avi' + ' -c:v libx264 -strict -2 ' + sys.path[
-        0] + '/video/finalOutput_VideoWithAudio.mp4'
+        0] + '/video/finalOutput_VideoWithAudio.mp4' + ' -y'
     os.system(cmd2)
 
 
+def delete(path):
+    try:
+        shutil.rmtree(path)
+        print('已删除：' + path)
+        return 0
+    except:
+        print('删除' + path + '失败，可能是权限不足或目录不存在')
+        return -1
+
+
+def input_process(default_video_path):
+    video_path = default_video_path
+    while not os.path.exists(video_path):
+        if len(sys.argv) == 2:
+            video_path = sys.argv[1]
+        elif len(sys.argv) == 1 and not os.path.exists(video_path):
+            video_path = input('请输入要处理的视频名称：')
+            video_path = sys.path[0] + '/' + video_path
+    return video_path
+
 if __name__ == '__main__':
-    video_path = sys.path[0] + '/test.flv'  # 把test.flv改成你的视频名字，注意前面的斜杠要保留
+    # 各种参数
+    video_path = sys.path[0] + '/input.mp4'  # 把input.mp4改成你的视频名字，注意前面的斜杠要保留
     save_pic_path = sys.path[0] + '/cache_pic'  # 别动
     save_charpic_path = sys.path[0] + '/cache_char'  # 别动
     processes_number = 8  # 使用多少个进程同时处理图片，通常不超过CPU线程数，可以自行设置
+    video_path = input_process(video_path)
 
+    # 处理
     vp = cv2.VideoCapture(video_path)
     number = video_to_pic(vp)
     FPS = vp.get(cv2.CAP_PROP_FPS)
@@ -220,4 +258,6 @@ if __name__ == '__main__':
         thread.join()
     vp.release()
     jpg_to_video(save_charpic_path, FPS)
+    delete(save_pic_path)
+    delete(save_charpic_path)
     write_audio(video_path)  # 把原视频的音频复制到新视频中。需要安装ffmpeg，否则报错。没有ffmpeg请注释掉这行代码。
